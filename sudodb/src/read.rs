3dbcb6800173772e531b2b7cc31c6e0e0c823267
Jordan Last <jordan.michael.last@gmail.com>
2021-05-25 17:25:58 -0600

    update sudodb read just a bit so that cross-relational filters by id work at least

--- sudodb/src/read.rs
+++ sudodb/src/read.rs
@@ -1,66 +1,57 @@
-use crate::{
-    ObjectTypeStore,
-    ReadInput,
-    FieldValuesStore,
-    FieldValueStore,
-    ReadInputOperation,
-    FieldTypesStore,
-    FieldType,
-    SudodbError,
-    FieldValue,
-    convert_field_value_store_to_json_string,
-    FieldValueScalar,
-    SelectionSet
-};
+// TODO clean up this whole file, it's really messy and confusing
+
 use chrono::prelude::{
     DateTime,
     Utc
 };
+use crate::{
+    convert_field_value_store_to_json_string,
+    FieldValue,
+    FieldValueScalar,
+    FieldValueStore,
+    FieldValuesStore,
+    FieldType,
+    FieldTypesStore,
+    get_object_type,
+    JSONString,
+    ObjectTypeStore,
+    ReadInput,
+    ReadInputOperation,
+    SelectionSet,
+    SudodbError
+};
 use std::error::Error;
-use std::collections::HashMap;
 
 const ERROR_PREFIX: &str = "Sudodb::read::error - ";
 
 // TODO prefix all errors with Sudodb::...or something like that
 // TODO let's try to make this the simplest experience ever
-// TODO consider Motoko integration...could we export this library such that Motoko could import it and use it?
-// TODO I think the hardest part will be sending data structures back and forth
-// TODO this is where all of the complexity will lie, or much of at at least
-// TODO we need to figure out how to enable amazing filtering capabilities
 pub fn read(
     object_type_store: &ObjectTypeStore,
     object_type_name: &str,
-    inputs: Vec<ReadInput>,
+    inputs: &Vec<ReadInput>,
     selection_set: &SelectionSet
-) -> Result<Vec<String>, Box<dyn Error>> { // TODO I think I want this to return a list of JSON strings...GraphQL can handle type checking the actual values I hope
-    let object_type_result = object_type_store.get(object_type_name);
+) -> Result<Vec<JSONString>, Box<dyn Error>> {
+    let object_type = get_object_type(
+        object_type_store,
+        String::from(object_type_name)
+    )?;
 
-    if let Some(object_type) = object_type_result {
-        let field_value_stores = find_field_value_stores_for_inputs(
-            &object_type.field_values_store,
-            &object_type.field_types_store,
-            &inputs
-        )?;
+    let field_value_stores = find_field_value_stores_for_inputs(
+        &object_type.field_values_store,
+        &object_type.field_types_store,
+        &inputs
+    )?;
 
-        let field_value_store_strings = field_value_stores.iter().map(|field_value_store| {
-            return convert_field_value_store_to_json_string(
-                object_type_store,
-                field_value_store,
-                selection_set
-            );
-        }).collect();
-    
-        return Ok(field_value_store_strings);
-    }
-    else {
-        return Err(Box::new(SudodbError {
-            message: format!(
-                "{error_prefix}Object type {object_type_name} not found in database",
-                error_prefix = ERROR_PREFIX,
-                object_type_name = object_type_name
-            )
-        }));
-    }
+    let field_value_store_strings = field_value_stores.iter().map(|field_value_store| {
+        return convert_field_value_store_to_json_string(
+            object_type_store,
+            field_value_store,
+            selection_set
+        );
+    }).collect();
+
+    return Ok(field_value_store_strings);
 }
 
 fn find_field_value_stores_for_inputs(
@@ -69,7 +60,7 @@ fn find_field_value_stores_for_inputs(
     inputs: &Vec<ReadInput>
 ) -> Result<Vec<FieldValueStore>, Box<dyn Error>> {
     // TODO I believe the result in the fold here needs to be mutable for efficiency...not sure, but perhaps
-    let temp: Result<Vec<FieldValueStore>, Box<dyn Error>> = field_values_store.values().try_fold(vec![], |mut result, field_value_store| {
+    let field_value_stores = field_values_store.values().try_fold(vec![], |mut result, field_value_store| {
         let inputs_match: bool = field_value_store_matches_inputs(
             field_value_store,
             field_types_store,
@@ -80,14 +71,14 @@ fn find_field_value_stores_for_inputs(
         if inputs_match == true {
             result.push(field_value_store.clone());
 
-            return Ok::<Vec<FieldValueStore>, Box<dyn Error>>(result);
+            return Ok(result);
         }
         else {
-            return Ok::<Vec<FieldValueStore>, Box<dyn Error>>(result);
+            return Ok(result);
         }
     });
 
-    return temp;
+    return field_value_stores;
 }
 
 fn field_value_store_matches_inputs(
@@ -135,11 +126,17 @@ fn field_value_store_matches_inputs(
         // TODO what if I split based on the scalar types here?
         if let (Some(field_type), Some(field_value)) = (field_type_option, field_value_option) {
             match field_type {
-                FieldType::RelationMany(object_type_name) => {
-                    return Ok(false);
+                FieldType::RelationMany(field_type_relation_info) => {
+                    return field_value_relation_many_matches_input(
+                        field_value,
+                        input
+                    );
                 },
-                FieldType::RelationOne(object_type_name) => {
-                    return Ok(false);
+                FieldType::RelationOne(field_type_relation_info) => {
+                    return field_value_relation_one_matches_input(
+                        field_value,
+                        input
+                    );
                 },
                 _ => {
                     return field_value_matches_input(
@@ -159,6 +156,178 @@ fn field_value_store_matches_inputs(
             }));
         }
     });
+}
+
+// TODO it really should not be that hard to have cross-relational filtering on all fields
+// TODO use recursion to get this done
+fn field_value_relation_many_matches_input(
+    field_value_relation_many: &FieldValue,
+    input: &ReadInput
+) -> Result<bool, Box<dyn Error>> {
+    match field_value_relation_many {
+        FieldValue::RelationMany(field_value_relation_many_option) => {
+            match &input.field_value {
+                // TODO we are only supporting filtering by id right now, this will look much different once we
+                // TODO add generic cross-relational filtering
+                // TODO do not implement cross-relational filtering until reading is cleaned up a lot
+                FieldValue::Scalar(input_field_value_scalar_option) => {
+                    // if let (
+                    //     Some(field_value_relation_many),
+                    //     Some(input_field_value_relation_many)
+                    // ) = (
+                    //     field_value_relation_many_option,
+                    //     input_field_value_relation_many_option
+                    // ) {
+                    //     return field_value_relation_many.
+                    // }
+                    // else {
+
+                    // }
+
+                    if let Some(field_value_relation_many) = field_value_relation_many_option {
+                        if let Some(input_field_value_scalar) = input_field_value_scalar_option {
+                            
+                            match input_field_value_scalar {
+                                FieldValueScalar::String(field_value_scalar_string) => {
+                                    return Ok(field_value_relation_many.relation_primary_keys.contains(field_value_scalar_string));
+                                },
+                                _ => {
+                                    return Ok(false);
+                                }
+                                // FieldValue::Scalar(test)
+                            };
+                        }
+                        else {
+                            return Ok(false);
+                        }
+                    }
+                    else {
+                        if let Some(input_field_value_scalar) = input_field_value_scalar_option {
+                            return Ok(false);
+                        }
+                        else {
+                            return Ok(true);
+                        }
+                    }
+                },
+                _ => {
+                    // TODO return an error
+                    return Ok(false);
+                }
+            };
+
+            // match &input.input_operation {
+            //     ReadInputOperation::Equals => {
+            //         match &input.field_value {
+            //             FieldValue::RelationMany(field_value_relation_many_option) => {
+            //                 // return field_value_relation_many_option == None;
+
+            //                 // if let Some(field_value_relation_many_option)
+            //             },
+            //             _ => {
+
+            //             }
+            //         };
+
+            //         // return 
+            //     },
+            //     _ => {
+            //         // TODO do something
+            //     }
+            // };
+        },
+        _ => {
+            // TODO I think this should panic?
+            return Ok(false);
+        }
+    };
+
+    // return Ok(true);
+}
+
+fn field_value_relation_one_matches_input(
+    field_value_relation_one: &FieldValue,
+    input: &ReadInput
+) -> Result<bool, Box<dyn Error>> {
+    match field_value_relation_one {
+        FieldValue::RelationOne(field_value_relation_one_option) => {
+            match &input.field_value {
+                // TODO we are only supporting filtering by id right now, this will look much different once we
+                // TODO add generic cross-relational filtering
+                // TODO do not implement cross-relational filtering until reading is cleaned up a lot
+                FieldValue::Scalar(input_field_value_scalar_option) => {
+                    // if let (
+                    //     Some(field_value_relation_many),
+                    //     Some(input_field_value_relation_many)
+                    // ) = (
+                    //     field_value_relation_many_option,
+                    //     input_field_value_relation_many_option
+                    // ) {
+                    //     return field_value_relation_many.
+                    // }
+                    // else {
+
+                    // }
+
+                    if let Some(field_value_relation_one) = field_value_relation_one_option {
+                        if let Some(input_field_value_scalar) = input_field_value_scalar_option {
+                            
+                            match input_field_value_scalar {
+                                FieldValueScalar::String(field_value_scalar_string) => {
+                                    return Ok(field_value_relation_one.relation_primary_key == String::from(field_value_scalar_string));
+                                },
+                                _ => {
+                                    return Ok(false);
+                                }
+                                // FieldValue::Scalar(test)
+                            };
+                        }
+                        else {
+                            return Ok(false);
+                        }
+                    }
+                    else {
+                        if let Some(input_field_value_scalar) = input_field_value_scalar_option {
+                            return Ok(false);
+                        }
+                        else {
+                            return Ok(true);
+                        }
+                    }
+                },
+                _ => {
+                    // TODO return an error
+                    return Ok(false);
+                }
+            };
+
+            // match &input.input_operation {
+            //     ReadInputOperation::Equals => {
+            //         match &input.field_value {
+            //             FieldValue::RelationMany(field_value_relation_many_option) => {
+            //                 // return field_value_relation_many_option == None;
+
+            //                 // if let Some(field_value_relation_many_option)
+            //             },
+            //             _ => {
+
+            //             }
+            //         };
+
+            //         // return 
+            //     },
+            //     _ => {
+            //         // TODO do something
+            //     }
+            // };
+        },
+        _ => {
+            // TODO I think this should panic?
+            return Ok(false);
+        }
+    };
+
+    // return Ok(true);
 }
 
 // TODO try to make this much less verbose if possible
@@ -242,9 +411,11 @@ fn field_value_matches_input(
             }
         },
         FieldValue::RelationMany(field_value_relation_many_option) => {
+            // TODO we might even want to panic here
             return Ok(false); // TODO relation filtering not yet implemented
         },
         FieldValue::RelationOne(field_value_relation_one_option) => {
+            // TODO we might even want to panic here
             return Ok(false); // TODO relation filtering not yet implemented
         }
     }
