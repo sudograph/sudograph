4b7530827c26e53d77251725b88b7f6faa56539d
Jordan Last <jordan.michael.last@gmail.com>
2021-06-01 18:29:15 -0500

    add untracked files, prepare deploy script

---
+++ deploy.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+read -p "Enter new version number:" VERSION
+
+echo -e "npm package: generator-sudograph, prepare and publish"
+
+cd generator-sudograph
+sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
+npm install
+npm publish
+cd ..
+
+echo -e "npm package: sudograph, prepare and publish"
+
+cd client
+sed -E -i "s/(\"version\": \")(.*)(\")/\1$VERSION\3/" package.json
+sed -E -i "s/(\"generator-sudograph\": \")(.*)(\")/\1$VERSION\3/" package.json
+npm install
+npm publish
+cd ..
+
+echo -e "crate: sudodb, prepare"
+
+cd sudodb
+sed -E -i "s/(^version = \")(.*)(\")/\1$VERSION\3/" Cargo.toml
+cargo publish --dry-run
+cd ..
+
+echo -e "crate: sudograph-generate, prepare"
+
+cd sudograph-generate
+sed -E -i "s/(^version = \")(.*)(\")/\1$VERSION\3/" Cargo.toml
+cargo publish --dry-run
+cd ..
+
+echo -e "crate: sudograph, prepare"
+
+sed -E -i "s/(^version = \")(.*)(\")/\1$VERSION\3/" Cargo.toml
+sed -E -i "s/(^sudodb = \{ version = \")(.*)(\", path = \"\.\/sudodb\" \})/\1$VERSION\3/" Cargo.toml
+sed -E -i "s/(^sudograph-generate = \{ version = \")(.*)(\", path = \"\.\/sudograph-generate\" \})/\1$VERSION\3/" Cargo.toml
+cargo publish --dry-run
+
+echo -e "commit and push final changes"
+
+git add --all
+git commit -am "updating to version $VERSION"
+git push origin main
+
+echo -e "create and push git tag"
+
+git tag v$VERSION
+git push origin v$VERSION
+
+echo -e "crate: sudodb, publish"
+
+cd sudodb
+cargo publish
+cd ..
+
+echo -e "crate: sudograph-generate, publish"
+
+cd sudograph-generate
+cargo publish
+cd ..
+
+echo -e "crate: sudograph, publish"
+
+cargo publish
+
+echo -e "All packages and crates have been published to version $VERSION"
