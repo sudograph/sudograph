b21a998aee6c10b0094a1c4957899cb83545a8b1
Jordan Last <jordan.michael.last@gmail.com>
2021-07-22 11:55:23 -0500

    delete tests are comprehensive and complete!

--- sudodb/src/delete.rs
+++ sudodb/src/delete.rs
@@ -1,8 +1,10 @@
 use crate::{
+    FieldType,
+    FieldValue,
     ObjectTypeStore,
-    SudodbError,
+    SelectionSet,
     convert_field_value_store_to_json_string,
-    SelectionSet
+    update::insert_field_value_relation_opposing_into_field_value_store
 };
 use std::error::Error;
 
@@ -13,72 +15,182 @@ pub fn delete(
     ids_option: Option<Vec<String>>,
     selection_set: &SelectionSet
 ) -> Result<Vec<String>, Box<dyn Error>> {
-    let object_type_option = object_type_store.get_mut(object_type_name);
+    // TODO playing around with where to place this to borrow correctly
+    remove_opposing_relation_ids(
+        object_type_store,
+        object_type_name,
+        &id_option,
+        &ids_option
+    )?;
 
-    if let Some(object_type) = object_type_option {
-        match id_option {
-            Some(id) => {
-                let field_value_store_option = object_type.field_values_store.get(&id);
+    let object_type = object_type_store.get_mut(object_type_name).ok_or("Should exist")?;
 
-                if let Some(field_value_store) = field_value_store_option {
+    match id_option {
+        Some(id) => {
+            let field_value_store_option = object_type.field_values_store.get(&id);
+
+            if let Some(field_value_store) = field_value_store_option {
+                let cloned = field_value_store.clone();
+    
+                object_type.field_values_store.remove(&id);
+    
+                let json_result_string = convert_field_value_store_to_json_string(
+                    object_type_store,
+                    &cloned,
+                    selection_set
+                );
+        
+                return Ok(vec![json_result_string]);
+            }
+            else {
+                return Ok(vec![]);
+            }
+        },
+        None => {
+            match ids_option {
+                Some(ids) => {
+                    let json_result_strings = ids.into_iter().fold(vec![], |result, id| {
+                        let object_type = object_type_store.get_mut(object_type_name).expect("should work, better error needed");
+                        let field_value_store_option = object_type.field_values_store.get(&id);
+
+                        if let Some(field_value_store) = field_value_store_option {
+                            
+                            let cloned = field_value_store.clone();
+                
+                            object_type.field_values_store.remove(&id);
+                
+                            let json_result_string = convert_field_value_store_to_json_string(
+                                object_type_store,
+                                &cloned,
+                                selection_set
+                            );
                     
-                    let cloned = field_value_store.clone();
-        
-                    object_type.field_values_store.remove(&id);
-        
-                    let json_result_string = convert_field_value_store_to_json_string(
-                        object_type_store,
-                        &cloned,
-                        selection_set
-                    );
-            
-                    return Ok(vec![json_result_string]);
-                }
-                else {
+                            return result.into_iter().chain(vec![json_result_string]).collect();
+                        }
+                        else {
+                            return result;
+                        }
+                    });
+
+                    return Ok(json_result_strings);
+                },
+                None => {
                     return Ok(vec![]);
                 }
-            },
-            None => {
-                match ids_option {
-                    Some(ids) => {
-                        let json_result_strings = ids.into_iter().fold(vec![], |result, id| {
-                            let object_type = object_type_store.get_mut(object_type_name).expect("should work, better error needed");
-                            let field_value_store_option = object_type.field_values_store.get(&id);
+            };
+        }
+    };
+}
 
-                            if let Some(field_value_store) = field_value_store_option {
-                                
-                                let cloned = field_value_store.clone();
-                    
-                                object_type.field_values_store.remove(&id);
-                    
-                                let json_result_string = convert_field_value_store_to_json_string(
-                                    object_type_store,
-                                    &cloned,
-                                    selection_set
-                                );
-                        
-                                return result.into_iter().chain(vec![json_result_string]).collect();
+fn remove_opposing_relation_ids(
+    object_type_store: &mut ObjectTypeStore,
+    object_type_name: &str,
+    id_option: &Option<String>,
+    ids_option: &Option<Vec<String>>
+) -> Result<(), Box<dyn Error>> {
+    if let Some(id) = id_option {
+        remove_opposing_relation_ids_for_id(
+            object_type_store,
+            object_type_name,
+            id
+        )?;
+    }
+
+    if let Some(ids) = ids_option {
+        for id in ids {
+            remove_opposing_relation_ids_for_id(
+                object_type_store,
+                object_type_name,
+                id
+            )?; 
+        }
+    }
+
+    return Ok(());
+}
+
+fn remove_opposing_relation_ids_for_id(
+    object_type_store: &mut ObjectTypeStore,
+    object_type_name: &str,
+    id: &str
+) -> Result<(), Box<dyn Error>> {
+    let object_type = object_type_store.get_mut(object_type_name).ok_or("Should exist")?;
+
+    let field_types_store = object_type.field_types_store.clone();
+    let field_value_store = object_type.field_values_store.get(id).ok_or("None")?.clone();
+
+    for field_value_tuple in field_value_store {
+        let field_value = field_value_tuple.1;
+        let field_type = field_types_store.get(&field_value_tuple.0).ok_or("None")?;
+    
+        match field_type {
+            FieldType::RelationMany((_, field_type_relation_many_info)) => {
+                if let Some(opposing_field_name) = &field_type_relation_many_info.opposing_field_name {
+                    let relation_primary_keys = match field_value {
+                        FieldValue::RelationMany(field_value_relation_many_option) => {
+                            if let Some(field_value_relation_many) = field_value_relation_many_option {
+                                Some(field_value_relation_many.relation_primary_keys)
                             }
                             else {
-                                return result;
+                                None
                             }
-                        });
+                        },
+                        _ => {
+                            return Err("must be a FieldValue::RelationOne".into());
+                        }
+                    };
 
-                        return Ok(json_result_strings);
-                    },
-                    None => {
-                        return Ok(vec![]);
+                    if relation_primary_keys == None {
+                        continue;
                     }
-                };
-            }
+
+                    for relation_primary_key in relation_primary_keys.ok_or("None")? {
+                        insert_field_value_relation_opposing_into_field_value_store(
+                            object_type_store,
+                            object_type_name,
+                            &field_type_relation_many_info,
+                            &opposing_field_name,
+                            &relation_primary_key,
+                            id,
+                            false
+                        )?;
+                    }
+                }
+            },
+            FieldType::RelationOne((_, field_type_relation_one_info)) => {
+                if let Some(opposing_field_name) = &field_type_relation_one_info.opposing_field_name {
+                    let relation_primary_key = match field_value {
+                        FieldValue::RelationOne(field_value_relation_one_option) => {
+                            if let Some(field_value_relation_one) = field_value_relation_one_option {
+                                Some(field_value_relation_one.relation_primary_key)
+                            }
+                            else {
+                                None
+                            }
+                        },
+                        _ => {
+                            return Err("must be a FieldValue::RelationOne".into());
+                        }
+                    };
+
+                    if relation_primary_key == None {
+                        continue;
+                    }
+
+                    insert_field_value_relation_opposing_into_field_value_store(
+                        object_type_store,
+                        object_type_name,
+                        &field_type_relation_one_info,
+                        &opposing_field_name,
+                        &relation_primary_key.ok_or("None")?,
+                        id,
+                        false
+                    )?;
+                }
+            },
+            _ => continue
         };
     }
-    else {
-        return Err(Box::new(SudodbError {
-            message: format!(
-                "{object_type_name} not found in database",
-                object_type_name = object_type_name
-            )
-        }));
-    }
+
+    return Ok(());
 }
--- sudograph-tests/src/arbitraries/queries/mutation_delete/mutation_delete.rs
+++ sudograph-tests/src/arbitraries/queries/mutation_delete/mutation_delete.rs
@@ -8,6 +8,9 @@ use crate::{
         }
     },
     utilities::graphql::{
+        get_field_by_field_name,
+        get_object_type_from_field,
+        get_opposing_relation_field,
         is_graphql_type_a_relation_many,
         is_graphql_type_a_relation_one,
         is_graphql_type_nullable
@@ -15,6 +18,7 @@ use crate::{
 };
 use graphql_parser::schema::{
     Document,
+    Field,
     ObjectType
 };
 use proptest::strategy::{
@@ -37,15 +41,17 @@ pub fn mutation_delete_arbitrary(
     return proptest::collection::vec(mutation_create_arbitrary, 1..5).prop_map(move |mutation_create_arbitrary_results| {
         let objects = mutation_create_arbitrary_results.iter().map(|mutation_create_arbitrary_result| {
             let object = create_and_retrieve_object(mutation_create_arbitrary_result.clone()).unwrap();
-
             return object;
         }).collect();
 
         let arbitrary_mutation_info = get_arbitrary_mutation_info(
+            graphql_ast,
             object_type,
             &objects
         );
         let arbitrary_query_infos = get_arbitrary_query_infos(
+            graphql_ast,
+            object_types,
             object_type,
             &objects
         );
@@ -58,7 +64,8 @@ pub fn mutation_delete_arbitrary(
 }
 
 fn get_arbitrary_mutation_info(
-    object_type: &ObjectType<String>,
+    graphql_ast: &'static Document<String>,
+    object_type: &'static ObjectType<String>,
     objects: &Vec<serde_json::value::Map<String, serde_json::Value>>
 ) -> ArbitraryMutationInfo {
     let mutation_name = format!(
@@ -76,6 +83,8 @@ fn get_arbitrary_mutation_info(
     let selection = "{ id }".to_string();
 
     let expected_value = get_arbitrary_mutation_info_expected_value(
+        graphql_ast,
+        object_type,
         objects,
         &mutation_name
     );
@@ -110,9 +119,26 @@ fn get_arbitrary_mutation_info_input_value(objects: &Vec<serde_json::value::Map<
 }
 
 fn get_arbitrary_mutation_info_expected_value(
+    graphql_ast: &'static Document<String>,
+    object_type: &'static ObjectType<String>,
     objects: &Vec<serde_json::value::Map<String, serde_json::Value>>,
     mutation_name: &str
 ) -> serde_json::value::Value {
+    if has_opposing_relation_one_non_nullable(
+        graphql_ast,
+        object_type,
+        objects
+    ) == true {
+        return serde_json::json!({
+            "data": null,
+            "errors": [
+                {
+                    "message": "Cannot set a non-nullable relation one to null"
+                }
+            ]
+        });
+    }
+
     if objects.len() == 1 {
         let object = objects.get(0).unwrap();
         let object_id = object.get("id").unwrap(); 
@@ -141,17 +167,91 @@ fn get_arbitrary_mutation_info_expected_value(
     }
 }
 
+fn has_opposing_relation_one_non_nullable(
+    graphql_ast: &'static Document<String>,
+    object_type: &'static ObjectType<String>,
+    objects: &Vec<serde_json::value::Map<String, serde_json::Value>>
+) -> bool {
+    let relation_one_non_nullable_fields: Vec<(&Field<String>, &String)> = objects.iter().map(|object| {
+        let relation_one_non_nullable_fields: Vec<(&Field<String>, &String)> = object
+            .keys()
+            .map(|key| {
+                let field = get_field_by_field_name(
+                    object_type,
+                    key
+                ).unwrap();
+
+                return (field, key);
+            })
+            .filter(|(field, key)| {
+                let opposing_field_option = get_opposing_relation_field(
+                    graphql_ast,
+                    field
+                );
+
+                if let Some(opposing_field) = opposing_field_option {
+                    return
+                        is_graphql_type_a_relation_one(
+                            graphql_ast,
+                            &opposing_field.field_type
+                        ) == true &&
+                        is_graphql_type_nullable(&opposing_field.field_type) == false &&
+                        (
+                            (
+                                object.get(*key).unwrap().as_array().is_some() &&
+                                object.get(*key).unwrap().as_array().unwrap().get(0).is_some() &&
+                                object.get(*key).unwrap().as_array().unwrap().get(0).unwrap().get("id").is_some()
+                            ) ||
+                            (
+                                object.get(*key).unwrap().get("id").is_some()
+                            )
+                        );
+                }
+                else {
+                    return false;
+                }
+            })
+            .collect();
+
+        return relation_one_non_nullable_fields;
+    })
+    .flatten()
+    .collect();
+
+    return relation_one_non_nullable_fields.len() != 0;
+}
+
 fn get_arbitrary_query_infos(
-    object_type: &ObjectType<String>,
+    graphql_ast: &'static Document<String>,
+    object_types: &'static Vec<ObjectType<String>>,
+    object_type: &'static ObjectType<String>,
     objects: &Vec<serde_json::value::Map<String, serde_json::Value>>
 ) -> Vec<ArbitraryQueryInfo> {
+    let has_opposing_relation_one_non_nullable = has_opposing_relation_one_non_nullable(
+        graphql_ast,
+        object_type,
+        objects
+    );
+
+    if has_opposing_relation_one_non_nullable == true {
+        return vec![];
+    }
+
     let object_arbitrary_query_info = get_object_arbitrary_query_infos(
+        object_type,
+        objects
+    );
+
+    let relations_arbitrary_query_infos = get_relations_arbitrary_query_infos(
+        graphql_ast,
+        object_types,
         object_type,
         objects
     );
     
     return vec![
-        object_arbitrary_query_info
+        object_arbitrary_query_info,
+        relations_arbitrary_query_infos
     ]
     .into_iter()
     .flatten()
@@ -197,4 +297,183 @@ fn get_object_arbitrary_query_infos(
             expected_value
         };
     }).collect();
+}
+
+fn get_relations_arbitrary_query_infos(
+    graphql_ast: &'static Document<String>,
+    object_types: &'static Vec<ObjectType<String>>,
+    object_type: &'static ObjectType<String>,
+    objects: &Vec<serde_json::value::Map<String, serde_json::Value>>
+) -> Vec<ArbitraryQueryInfo> {
+    return objects.iter().map(|object| {
+        let arbitrary_query_infos: Vec<ArbitraryQueryInfo> = object
+            .keys()
+            .map(|key| {
+                let field = get_field_by_field_name(
+                    object_type,
+                    key
+                ).unwrap();
+
+                return (field, key);
+            })
+            .filter(|(field, key)| {
+                return
+                    (is_graphql_type_a_relation_many(
+                        graphql_ast,
+                        &field.field_type
+                    ) == true ||
+                    is_graphql_type_a_relation_one(
+                        graphql_ast,
+                        &field.field_type
+                    ) == true) &&
+                    object.get(*key).is_some() &&
+                    (
+                        (
+                            object.get(*key).unwrap().as_array().is_some() &&
+                            object.get(*key).unwrap().as_array().unwrap().get(0).is_some() &&
+                            object.get(*key).unwrap().as_array().unwrap().get(0).unwrap().get("id").is_some()
+                        ) ||
+                        (
+                            object.get(*key).unwrap().get("id").is_some()
+                        )
+                    );
+            })
+            .map(|(field, key)| {
+                if is_graphql_type_a_relation_many(
+                    graphql_ast,
+                    &field.field_type
+                ) == true {
+                    let opposing_object_id = object.get(key).unwrap().as_array().unwrap().get(0).unwrap().get("id").unwrap();
+
+                    return (
+                        field,
+                        opposing_object_id
+                    );
+                }
+                else {
+                    let opposing_object_id = object.get(key).unwrap().get("id").unwrap();
+
+                    return (
+                        field,
+                        opposing_object_id
+                    );
+                }
+            })
+            .map(|(field, opposing_object_id)| {
+                let opposing_object_type = get_object_type_from_field(
+                    object_types,
+                    &field
+                ).unwrap();
+
+                let query_name = format!(
+                    "read{opposing_object_type_name}",
+                    opposing_object_type_name = opposing_object_type.name
+                );
+            
+                let search_variable_type = format!(
+                    "Read{opposing_object_type_name}Input!",
+                    opposing_object_type_name = opposing_object_type.name
+                );
+                        
+                let search_value = serde_json::json!({
+                    "id": {
+                        "eq": opposing_object_id
+                    }
+                });
+
+                let opposing_field_option = get_opposing_relation_field(
+                    graphql_ast,
+                    field
+                );
+
+                let selection = get_relations_arbitrary_query_infos_selection(&opposing_field_option);
+            
+                let expected_value = get_relations_arbitrary_query_infos_expected_value(
+                    graphql_ast,
+                    &opposing_field_option,
+                    opposing_object_id,
+                    &query_name
+                );
+            
+                return ArbitraryQueryInfo {
+                    query_name,
+                    search_variable_type,
+                    search_value,
+                    selection,
+                    expected_value
+                };
+            })
+            .collect();
+
+        return arbitrary_query_infos;
+    })
+    .flatten()
+    .collect();
+}
+
+fn get_relations_arbitrary_query_infos_selection(opposing_field_option: &Option<Field<String>>) -> String {
+    if let Some(opposing_field) = opposing_field_option {
+        let opposing_field_name = &opposing_field.name;
+        
+        return format!(
+            "{{
+                id
+                {opposing_field_name} {{
+                    id
+                }}
+            }}",
+            opposing_field_name = opposing_field_name
+        );
+    }
+    else {
+        return format!(
+            "{{
+                id
+            }}"
+        );
+    }
+}
+
+fn get_relations_arbitrary_query_infos_expected_value(
+    graphql_ast: &'static Document<String>,
+    opposing_field_option: &Option<Field<String>>,
+    opposing_object_id: &serde_json::value::Value,
+    query_name: &str
+) -> serde_json::value::Value {
+    if let Some(opposing_field) = opposing_field_option {
+        let opposing_field_name = &opposing_field.name;
+
+        if is_graphql_type_a_relation_many(
+            graphql_ast,
+            &opposing_field.field_type
+        ) == true {
+            return serde_json::json!({
+                "data": {
+                    query_name: [{
+                        "id": opposing_object_id,
+                        opposing_field_name: []
+                    }]
+                }
+            });
+        }
+        else {
+            return serde_json::json!({
+                "data": {
+                    query_name: [{
+                        "id": opposing_object_id,
+                        opposing_field_name: null
+                    }]
+                }
+            });
+        }
+    }
+    else {
+        return serde_json::json!({
+            "data": {
+                query_name: [{
+                    "id": opposing_object_id
+                }]
+            }
+        });
+    }
 }
